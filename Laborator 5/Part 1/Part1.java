575544e5a1bedd01b3851e114c773585de032f75
Victoras23 <87285293+Victoras23@users.noreply.github.com>
2021-11-07 16:29:00 +0200

    Add files via upload

--- Laborator 5/Part 1/Part1.java
+++ Laborator 5/Part 1/Part1.java
@@ -10,24 +10,24 @@ public class Part1 {
 		H h=new H("a","x","b","c","d","e","f","g","h");
 		I i=new I("a","x","b","c","d","e","f","g","h","i");
 		J j=new J("a","x","b","c","d","e","f","g","h","i","j");
-		System.out.println("Class A : a: "+a.a);
-		System.out.println("Class B : a: "+b.a+" b: "+b.b);
-		System.out.println("Class C : a: "+c.a+" b: "+c.b+" c: "+c.c);
-		System.out.println("Class D : a: "+d.a+" b: "+d.b+" c: "+d.c+" d: "+d.d);
-		System.out.println("Class E : a: "+e.a+" b: "+e.b+" c: "+e.c+" d: "+e.d+" e: "+e.e);
-		System.out.println("Class F : a: "+f.a+" b: "+f.b+" c: "+f.c+" d: "+f.d+" e: "+f.e+" f: "+f.f);
-		System.out.println("Class G : a: "+g.a+" b: "+g.b+" c: "+g.c+" d: "+g.d+" e: "+g.e+" f: "+g.f+" g: "+g.g);
-		System.out.println("Class H : a: "+h.a+" b: "+h.b+" c: "+h.c+" d: "+h.d+" e: "+h.e+" f: "+h.f+" g: "+h.g+" h: "+h.h);
-		System.out.println("Class I : a: "+i.a+" b: "+i.b+" c: "+i.c+" d: "+i.d+" e: "+i.e+" f: "+i.f+" g: "+i.g+" h: "+i.h+" i: "+i.i);
-		System.out.println("Class J : a: "+j.a+" b: "+j.b+" c: "+j.c+" d: "+j.d+" e: "+j.e+" f: "+j.f+" g: "+j.g+" h: "+j.h+" i: "+j.i+" j: "+j.j);
+		System.out.println("Class A : a: "+a.a+" x: "+a.x.x);
+		System.out.println("Class B : a: "+b.a+" b: "+b.b+" x: "+b.x.x);
+		System.out.println("Class C : a: "+c.a+" b: "+c.b+" c: "+c.c+" x: "+c.x.x);
+		System.out.println("Class D : a: "+d.a+" b: "+d.b+" c: "+d.c+" d: "+d.d+" x: "+d.x.x);
+		System.out.println("Class E : a: "+e.a+" b: "+e.b+" c: "+e.c+" d: "+e.d+" e: "+e.e+" x: "+e.x.x);
+		System.out.println("Class F : a: "+f.a+" b: "+f.b+" c: "+f.c+" d: "+f.d+" e: "+f.e+" f: "+f.f+" x: "+f.x.x);
+		System.out.println("Class G : a: "+g.a+" b: "+g.b+" c: "+g.c+" d: "+g.d+" e: "+g.e+" f: "+g.f+" g: "+g.g+" x: "+g.x.x);
+		System.out.println("Class H : a: "+h.a+" b: "+h.b+" c: "+h.c+" d: "+h.d+" e: "+h.e+" f: "+h.f+" g: "+h.g+" h: "+h.h+" x: "+h.x.x);
+		System.out.println("Class I : a: "+i.a+" b: "+i.b+" c: "+i.c+" d: "+i.d+" e: "+i.e+" f: "+i.f+" g: "+i.g+" h: "+i.h+" i: "+i.i+" x: "+i.x.x);
+		System.out.println("Class J : a: "+j.a+" b: "+j.b+" c: "+j.c+" d: "+j.d+" e: "+j.e+" f: "+j.f+" g: "+j.g+" h: "+j.h+" i: "+j.i+" j: "+j.j+" x: "+j.x.x);
 	}
 }
 class A{
 	protected String a;
-	protected X x;
+	protected X x = new X("x");
 	A(String a , String x){
 		this.a=a;
-		this.x= new X(x);
+		this.x= new X("x");
 	}
 }
 
