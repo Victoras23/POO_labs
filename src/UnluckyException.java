02068fa0fbec4028a606589643fd673e47fbef04
Victoras23 <87285293+Victoras23@users.noreply.github.com>
2021-11-28 03:05:27 +0200

    Delete src directory

--- src/UnluckyException.java
+++
@@ -1,9 +0,0 @@
-public class UnluckyException extends Exception{
-    UnluckyException(String in1, String in2) {
-        super(in1, in2);
-    }
-    String exception(){
-        if(input2.equals("13"))return"It is unlucky number";
-        else return "";
-    }
-}
